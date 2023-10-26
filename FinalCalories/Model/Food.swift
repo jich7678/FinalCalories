f13075923c9ad187b035d249fbcbc77629502e80
Jiawei Chen <jich7678@colorado.edu>
2023-10-26 02:45:50 -0600

    API

---
+++ FinalCalories/Model/Food.swift
@@ -0,0 +1,8 @@
+//
+//  Food.swift
+//  FinalCalories
+//
+//  Created by Sam Chen on 10/26/23.
+//
+
+import Foundation
---
+++ FinalCalories/ViewModel/FoodViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FoodViewModel.swift
+//  FinalCalories
+//
+//  Created by Sam Chen on 10/26/23.
+//
+
+import Foundation
