f9d18d931517011a5f0791bc50f795867b7ff2c1
Jiawei Chen <jich7678@colorado.edu>
2023-10-26 01:15:19 -0600

    Initial Commit

---
+++ FinalCalories/FinalCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalCaloriesApp.swift
+//  FinalCalories
+//
+//  Created by Sam Chen on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
